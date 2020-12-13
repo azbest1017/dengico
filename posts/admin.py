5ddd324a8c87fffa8557a615477933d662de5ac8
Azbest <azbest1017@gmail.com>
2020-12-13 13:54:12 +0600

    admin normal

--- posts/admin.py
+++ posts/admin.py
@@ -5,6 +5,6 @@ from .models import Post, Affiliate_Partner, Reviews, Methot_Withdraw
 admin.site.register(Post)
 admin.site.register(Affiliate_Partner)
 admin.site.register(Reviews)
-admin.site.register(Methot_Withdrasdaw)
+admin.site.register(Methot_Withdraw)
 
 from ckeditor.widgets import CKEditorWidget
