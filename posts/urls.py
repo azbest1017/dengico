74fefcba8489152e042b6815cf97739f8e51c694
Azbest <azbest1017@gmail.com>
2020-12-14 22:45:59 +0600

    My First Filter

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
 #from . import views
-from .views import HomeView, PostDetails, Redirect
+from .views import MfoView, PostDetails, Redirect
 urlpatterns = [
-    #path('', views.home, name="home"),
-    path('', HomeView.as_view(), name="home"),
+    path('', MfoView , name='home'),
     path('мфо/<str:slug>', PostDetails.as_view(), name='post-details'),
     path('go/<str:slug>', Redirect.as_view(), name='redirect_go')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Q, Count
 from django.views.generic import ListView, DetailView
 from .models import Post, Methot_Withdraw
 #def home(request):
 #    return render(request, 'home.html', {})
-class HomeView(ListView):
-     model = Post
-     template_name = 'home.html'
-     ordering = ['-pament','-scope']
+#class HomeView(ListView):
+#     model = Post
+#     template_name = 'home.html'
+    # ordering = ['-pament','-scope']
 
 class PostDetails(DetailView):
     model = Post
@@ -15,4 +16,18 @@ class PostDetails(DetailView):
 class Redirect(DetailView):
     model = Post
     template_name = 'redirect.html'
-    
+
+def filter(request):
+    all_mfo = Post.objects.all()
+    first_credit_free = request.GET.get('first_credit_free')
+    if first_credit_free == 'on':
+        all_mfo = all_mfo.filter(first_credit_free=True)
+        print(all_mfo)
+    return all_mfo
+
+def MfoView(request):
+    all_mfo = filter(request)
+    context = {
+        'posts': all_mfo.order_by('-pament','-scope')
+    }
+    return render(request, "home.html", context)
