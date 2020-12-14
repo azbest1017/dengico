b7c358a7dc16cb32236b342aca6595d71c2af4de
Azbest <azbest1017@gmail.com>
2020-12-14 20:40:37 +0600

    1

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import HomeView, PostDetails, Redirect
 urlpatterns = [
     #path('', views.home, name="home"),
-    path('',HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name="home"),
     path('мфо/<str:slug>', PostDetails.as_view(), name='post-details'),
     path('go/<str:slug>', Redirect.as_view(), name='redirect_go')
 ]
